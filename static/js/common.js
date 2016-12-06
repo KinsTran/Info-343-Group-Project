5ab89a4971ac7ba25ef5429b583baf65a6b88e8c
Henry Zhang <henry.zhang@live.com>
2016-12-06 15:02:22 -0800

    add firebase config

---
+++ static/js/common.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var config = {
+    apiKey: "AIzaSyBRPSduRq2PsdIuXrLdBNl1RYU0lhYQMWE",
+    authDomain: "bonseye-56d35.firebaseapp.com",
+    databaseURL: "https://bonseye-56d35.firebaseio.com",
+    storageBucket: "bonseye-56d35.appspot.com",
+    messagingSenderId: "827229640305"
+};
+firebase.initializeApp(config);
