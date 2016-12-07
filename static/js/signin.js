29f596ff0da9b23e66753b9fcb7eb7a174a73c94
Henry Zhang <henry.zhang@live.com>
2016-12-07 14:08:03 -0800

    add signin functionality

--- static/js/signin.js
+++ static/js/signin.js
@@ -1,1 +1,23 @@
 "use strict";
+
+var signInForm = document.getElementById("sign-in-form");
+var userEmail = document.getElementById("email");
+var userPassword = document.getElementById("password");
+var errorBox = document.getElementById("error");
+
+signInForm.addEventListener("submit", function(evt) {
+    errorBox.innerHTML = "";
+    evt.preventDefault();
+
+    firebase.auth().signInWithEmailAndPassword(userEmail.value, userPassword.value)
+    .then(function() {
+        window.location = "app.html";
+    })
+    .catch(function(error) {
+        var p = document.createElement("p");
+        p.textContent = error;
+        errorBox.appendChild(p);
+    });
+
+    return false;
+});
