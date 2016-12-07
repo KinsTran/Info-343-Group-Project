f84845717da38b6659c0108e34223a283a5ba523
Henry Zhang <henry.zhang@live.com>
2016-12-07 14:01:14 -0800

    add error display for user

--- static/js/index.js
+++ static/js/index.js
@@ -5,21 +5,33 @@ var userEmail = document.getElementById("email");
 var userName = document.getElementById("display");
 var userPassword = document.getElementById("password");
 var userConfirmation = document.getElementById("confirm");
+var errorBox = document.getElementById("error");
 
 signUpForm.addEventListener("submit", function(evt) {
+    // clear out previous error message
+    errorBox.innerHTML = "";
     evt.preventDefault();
-    firebase.auth().createUserWithEmailAndPassword(userEmail.value, userPassword.value)
-    .then(function(user) {
-        return user.updateProfile({
-            displayName: userName.value
+    if (userPassword.value == userConfirmation.value) {
+        firebase.auth().createUserWithEmailAndPassword(userEmail.value, userPassword.value)
+        .then(function(user) {
+            return user.updateProfile({
+                displayName: userName.value
+            });
+        })
+        .then(function() {
+            window.location = "app.html";
+        })
+        .catch(function(error) {
+            // let user know why they can't sign up
+            var p = document.createElement("p");
+            p.textContent = error;
+            errorBox.appendChild(p);
         });
-    })
-    .then(function() {
-        window.location = "app.html";
-    })
-    .catch(function(error) {
-        console.log(error.message);
-    });
+    } else {
+        var p = document.createElement("p");
+        p.textContent = "Make sure your password inputs match!";
+        errorBox.appendChild(p);
+    }
 
     return false;
-})
+});
