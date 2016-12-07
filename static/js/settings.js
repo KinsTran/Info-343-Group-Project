d0e72f59a9e8acfb44fd2523831bacf3cfbbf1e4
Kinsley <kins.tran@comcast.net>
2016-12-06 22:55:59 -0800

    Some work done on settings, commiting to pull later. Must find way to pull json from yelp website, possibly Fetch

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,16 +1,31 @@
 // Code for configuring settings on settings page
 "use strict";
 
+var TESTUSER = true; // FOR TESTING PURPOSES TAKE OUT LATER
 var currentUser;
 var signOutButton = document.getElementById("signOut"); // NEEDS TO HAVE SIGNOUT BUTTON
 
+//MAY NEED FETCH FOR THIS TO WORK
+var categories = JSON.parse("https://www.yelp.com/developers/documentation/v2/all_category_list/categories.json");
+//http://stackoverflow.com/questions/45015/safely-turning-a-json-string-into-an-object Forgot all about this to be honest
+
+var restuarants = new Array;
+// country_whitelist must include "US", parents must include "restaurants"
+
+
 firebase.auth().onAuthStateChanged(function(user){
-    if(user) {
+    if(TESTUSER) {
         currentUser = user;
+        categories.forEach(function(place) {
+            if(place.parents.includes("restuarants") && place.country_whitelist.includes("US")) {
+                restuarants.push(place.title);
+            }
+        })
     } else {
         location = "index.html"; // Redirect to index if navigates to settings without being logged in
     }
-})
+    console.log(restuarants);
+}) 
 
 signOutButton.addEventListener("click", function() {
     firebase.auth().signOut();
