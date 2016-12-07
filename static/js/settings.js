06bc73a902e3786e544fff4dfa867703f75c4719
Kinsley <kins.tran@comcast.net>
2016-12-06 23:29:44 -0800

    Struggling with getting data. Either scraping data from Yelp, or using a file we have. Possibly need other apis for either

--- static/js/settings.js
+++ static/js/settings.js
@@ -5,8 +5,11 @@ var TESTUSER = true; // FOR TESTING PURPOSES TAKE OUT LATER
 var currentUser;
 var signOutButton = document.getElementById("signOut"); // NEEDS TO HAVE SIGNOUT BUTTON
 
-//MAY NEED FETCH FOR THIS TO WORK
-var categories = JSON.parse("https://www.yelp.com/developers/documentation/v2/all_category_list/categories.json");
+const YELPCAT = "https://www.yelp.com/developers/documentation/v2/all_category_list/categories.json";
+
+//TEMPORARY HARD CODE, FETCH LATER
+var tempCategories = 
+var categories = JSON.parse(tempCategories);
 //http://stackoverflow.com/questions/45015/safely-turning-a-json-string-into-an-object Forgot all about this to be honest
 
 var restuarants = new Array;
@@ -30,5 +33,3 @@ firebase.auth().onAuthStateChanged(function(user){
 signOutButton.addEventListener("click", function() {
     firebase.auth().signOut();
 })
-
-// https://github.com/KinsTran/info343-in-class/blob/master/node/server.js for yelp api stuff
