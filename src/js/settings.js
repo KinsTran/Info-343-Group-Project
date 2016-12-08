2ae874b74503e8ac12b08b6f709e9e138969b437
Kinsley <kins.tran@comcast.net>
2016-12-07 22:25:08 -0800

    Some styling done

--- src/js/settings.js
+++ src/js/settings.js
@@ -4,11 +4,12 @@
 var TESTUSER = true; // FOR TESTING PURPOSES TAKE OUT LATER
 var currentUser;
 var signOutButton = document.getElementById("signOut"); // NEEDS TO HAVE SIGNOUT BUTTON
+var options = document.getElementById("options");
 
 const YELPCATEGORIES = "https://www.yelp.com/developers/documentation/v2/all_category_list/categories.json";
 
 //TEMPORARY HARD CODE, FETCH LATER
-var tempCategories = require("../../categories.json")
+var tempCategories = require("../../categories.json") // CANNOT GET REQUIRE TO WORK FIX THIS
 var categories = JSON.parse(tempCategories);
 //http://stackoverflow.com/questions/45015/safely-turning-a-json-string-into-an-object Forgot all about this to be honest
 
@@ -25,12 +26,22 @@ firebase.auth().onAuthStateChanged(function(user){
                 restuarants.push(place.title);
             }
         })
-    } else {
-        location = "index.html"; // Redirect to index if navigates to settings without being logged in
+        renderOptions();
+
+    } else { // Redirect to index if navigates to settings without being logged in OR if user logs out
+        location = "index.html";
     }
     console.log(restuarants);
 }) 
 
-signOutButton.addEventListener("click", function() {
+signOutButton.addEventListener("click", function() { // Signs out for user if clicked
     firebase.auth().signOut();
 })
+
+function renderOptions() { // Added for clarity's sake
+    restuarants.forEach(function() {
+        var button = document.createElement("div");
+        button.classList.add("button", "shadow-drama");
+        
+    })
+}
