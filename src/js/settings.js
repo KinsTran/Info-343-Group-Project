98a09a26d5d5fec9b2d403b1971740b29f1cba9a
Kinsley <kins.tran@comcast.net>
2016-12-10 00:56:29 -0800

    Preference Buttons working

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,6 +2,7 @@
 "use strict";
 
 var currentUser;
+var currentRef;
 var signOutButton = document.getElementById("signOut"); 
 var returnButton = document.getElementById("saveAndReturn");
 var options = document.getElementById("options");
@@ -11,6 +12,7 @@ var restaurants = new Array;
 firebase.auth().onAuthStateChanged(function(user){
     if(user) { 
         currentUser = user;
+        currentRef = database.ref("settings/" + currentUser.uid);
         categories.forEach(function(place) {
             if(place.parents.includes("restaurants") && (place.country_whitelist && place.country_whitelist.includes("US") || !place.country_whitelist)) {
                 restaurants.push(place.title);
@@ -28,38 +30,31 @@ signOutButton.addEventListener("click", function() { // Signs out for user if cl
 
 function renderOptions(snapshot) { // Added for clarity's sake, instead of just a raw function
     options.innerHTML = "";
-    var userPrefs = userSettingsRef.child(currentUser.uid).child("prefs");
-    console.log("i made it here")
+ /*   if(null == snapshot.child(currentUser.uid)) { // Creates child object if user does not have a 'preferences' setting yet
+        snapshot.child(currentUser.uid).set(new Object);
+    } */
+
     restaurants.forEach(function(type) {
         var button = document.createElement("div");
         button.classList.add("button", "shadow-drama", "default-font");
-        button.textContent = type; // CHECK ALL SNAPSHOT RELATED THINGS, PROBABLY DID THEM WRONG
+        button.textContent = type;
+        if(type == snapshot.child(currentUser.uid).child(type).val()) { // If restuarant type is in database (selected)
+            button.classList.add("selected");
+        }
 
-        var keys = Object.keys(snapshot);
-
-        keys.forEach(function(key) {
-            
-            if(snapshot[key] == type) {
-                button.classList.add("selected");
+        button.addEventListener("click", function(event) {
+            event.preventDefault();
+            console.log(snapshot.child(currentUser.uid).val());
+            if (button.classList.contains("selected")) { // If the preference is selected, toggle off
+                var preferenceType = new Object;
+                preferenceType[type] = null;
+                currentRef.update(preferenceType); // If preference is not selected, toggle on
+            } else { // http://stackoverflow.com/questions/11508463/javascript-set-object-key-by-variable
+                var preferenceType = new Object;
+                preferenceType[type] = type;
+                currentRef.update(preferenceType);
             }
-            
-            button.addEventListener("click", function(event) {
-                event.preventDefault();
-
-                if (button.classList.contains("selected")) {
-                    button.classList.remove("selected");
-                    // keys.forEach(function(key) {
-                    //     if (userPrefs[key] == type) {
-                    //         userPrefs.remove(key);
-                    //     }
-                    // })
-                } else {
-                    button.classList.add("selected");
-                    userPrefs.push(type);
-            }
-            
             return false;
-            });
         });
 
         options.appendChild(button);
@@ -67,7 +62,8 @@ function renderOptions(snapshot) { // Added for clarity's sake, instead of just 
 }
 
 function render(snapshot) {
-    snapshot.forEach(renderOptions);
+    console.log("Called");
+    renderOptions(snapshot);
 }
 
 userSettingsRef.on("value", render); // Rerendering all buttons for sake of one button changing value
